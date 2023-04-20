50bd6431cf91cbb7b60e06240f53dfd31c574cf9
Diogo Borges Rodrigues <62111077+dihbgs@users.noreply.github.com>
2023-04-20 15:12:21 -0300

    Update Vehicle.java

--- Vehicle.java
+++ Vehicle.java
@@ -1,8 +1,22 @@
 public class Vehicle {
-  private static final float fuelCost = 0.55f;
+  private static final float FUEL_COST = 0.55f;
+  private static final String[] MODEL_ONE = {
+    "    ____\n",
+    " __/  |_ \\_\n",
+    "|  _     _``-.\n",
+    "'-(_)---(_)--'\n\n"
+  };
 
-  private final Tire[] tires;
-  private final int id;
+  private static final String[] MODEL_TWO = {
+    "  ______\n",
+    " /|_||_\\`.__\n",
+    "(   _    _ _\\ \n",
+    "=`-(_)--(_)-'\n\n"
+  };
+
+  private final String[] MODEL;
+  private final Tire[] TIRES;
+  private final int ID;
 
   private float fuel;
   private int distance;
@@ -11,26 +25,62 @@ public class Vehicle {
   public Vehicle() {
     this.fuel = 2.5f;
     this.distance = 0;
-    this.tires = new Tire[4];
+    this.TIRES = new Tire[4];
     this.hasIPVA = (Math.random() > 0.5);
-    this.id = (this.tires.hashCode() * this.getHashSum()) % 100000;
+    this.MODEL = (Math.random() > 0.5) ? Vehicle.MODEL_ONE : Vehicle.MODEL_TWO;
+
+    for(int i=0; i<4; i++) {
+      this.TIRES[i] = new Tire();
+    }
+
+    this.ID = (this.TIRES.hashCode() * this.getHashSum()) % 100000;
   }
 
-  public static void main(String[] args) {
-    Vehicle test = new Vehicle();
+  public void move() {
+    if(!this.canMove()) {
+      return;
+    }
 
-    test.
+    this.distance += 1;
   }
 
-  public get
+  private boolean hasEnoughFuel() {
+    return this.fuel >= Vehicle.FUEL_COST;
+  }
+
+  private boolean isFullyCalibrated() {
+    for(int i=0; i<4; i++) {
+      if(!this.TIRES[i].getCalibration()) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  private boolean canMove() {
+    return !(this.hasEnoughFuel() && this.isFullyCalibrated() && this.hasIPVA);
+  }
+
+  public void draw() {
+    String blocksMoved = "";
+
+    for(int i=0; i<distance; i++) {
+      blocksMoved += "     ";
+    }
+
+    for(int i=0; i<4; i++) {
+      System.out.print(blocksMoved + this.MODEL[i]);
+    }
+  }
 
   private int getHashSum() {
     int sum = 0;
     Tire currentTire;
 
     for(int i=0; i<4; i++) {
-      currentTire = this.tires[i];
-      sum += currentTire.hashCode() * currentTire.isCalibrated;
+      currentTire = this.TIRES[i];
+      sum += currentTire.hashCode() * (currentTire.getCalibration() ? 1 : 0);
     }
 
     return sum;
