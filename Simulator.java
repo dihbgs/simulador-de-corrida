4429da52fbe16617254f8cb0a3685a578a597e2e
dihbgs <dihborgesdr@gmail.com>
2023-04-29 02:52:21 -0300

    add javadoc

--- Simulator.java
+++ Simulator.java
@@ -6,19 +6,43 @@ import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
-import java.lang.SuppressWarnings;
-
+/**
+ * Classe que representa um simulador de veiculos.
+ * 
+ * @see Vehicle
+ */
 public class Simulator {
   private ArrayList<Vehicle> vehicles;
 
+  /**
+   * Esse metodo e utilizado para criar um novo simulador.
+   */
   public Simulator() {
     this.vehicles = new ArrayList<Vehicle>();
   }
 
+  /**
+   * Esse metodo e utilizado para adicionar um novo veiculo ao simulador.
+   * 
+   * @param vehicle - Veiculo a ser adicionado.
+   * 
+   * @return void
+   * 
+   * @see Vehicle
+   */
   public void addVehicle(Vehicle vehicle) {
     this.vehicles.add(vehicle);
   }
 
+  /**
+   * Esse metodo e utilizado para remover um veiculo do simulador.
+   * 
+   * @param int - ID do veiculo a ser removido.
+   * 
+   * @return void
+   * 
+   * @see Vehicle
+   */
   public void removeVehicle(int id) {
     Vehicle vehicle = this.getVehicleById(id);
 
@@ -29,6 +53,16 @@ public class Simulator {
     this.vehicles.remove(vehicle);
   }
 
+  /**
+   * Esse metodo e utilizado para reabastecer um veiculo.
+   * 
+   * @param int   - ID do veiculo a ser reabastecido.
+   * @param float - Quantidade de combustivel a ser abastecido.
+   * 
+   * @return void
+   * 
+   * @see Vehicle
+   */
   public void refuelVehicle(int id, float fuel) {
     Vehicle vehicle = this.getVehicleById(id);
 
@@ -39,6 +73,15 @@ public class Simulator {
     vehicle.refuel(fuel);
   }
 
+  /**
+   * Esse metodo e utilizado para mover um veiculo.
+   * 
+   * @param int - ID do veiculo a ser movido.
+   * 
+   * @return void
+   * 
+   * @see Vehicle
+   */
   public void moveVehicle(int id) {
     Vehicle vehicle = this.getVehicleById(id);
 
@@ -49,13 +92,29 @@ public class Simulator {
     vehicle.move();
   }
 
+  /**
+   * Esse metodo e utilizado para mover todos os veiculos.
+   * 
+   * @return void
+   * 
+   * @see Vehicle
+   */
   public void moveAllVehicles() {
     for (Vehicle vehicle : this.vehicles) {
       vehicle.move();
     }
   }
 
-  public void printVehicle(int id, boolean printHeader) {
+  /**
+   * Esse metodo e utilizado para imprimir os dados de um veiculo.
+   * 
+   * @param int - ID do veiculo a ser impresso.
+   * 
+   * @return void
+   * 
+   * @see Vehicle
+   */
+  public void printVehicle(int id) {
     Vehicle vehicle = this.getVehicleById(id);
 
     if (vehicle == null) {
@@ -67,6 +126,13 @@ public class Simulator {
     System.out.println();
   }
 
+  /**
+   * Esse metodo e utilizado para imprimir os dados de todos os veiculos.
+   * 
+   * @return void
+   * 
+   * @see Vehicle
+   */
   public void printAllVehicles() {
     printHeader();
     for (Vehicle vehicle : this.vehicles) {
@@ -75,9 +141,19 @@ public class Simulator {
     System.out.println();
   }
 
+  /**
+   * Esse metodo e utilizado internamente pelo simulador para auxiliar na
+   * formatacao dos dados.
+   * 
+   * @param Vehicle - Veiculo a ser impresso.
+   * @param int     - Ordem do veiculo na lista.
+   * 
+   * @return void
+   */
   private void printData(Vehicle vehicle, int order) {
     System.out
-        .printf("   %d\t| #%d\t| \t%.2f\t| %s", order, vehicle.getId(), vehicle.getFuel(), (vehicle.getIpva() ? "Sim" : "Nao"));
+        .printf("   %d\t| #%d\t| \t%.2f\t| %s", order, vehicle.getId(), vehicle.getFuel(),
+            (vehicle.getIpva() ? "Sim" : "Nao"));
 
     for (int i = 0; i < 4; i++) {
       System.out.print("\t| " + (vehicle.getTire(i).getCalibration() ? "Calibrado" : "Vazio\t"));
@@ -86,22 +162,51 @@ public class Simulator {
     System.out.println();
   }
 
+  /**
+   * Esse metodo e utilizado internamente pelo simulador para auxiliar na
+   * formatacao dos dados.
+   * 
+   * @return void
+   */
   private void printHeader() {
     System.out.println(" Nº\t| ID\t| Combustivel\t| IPVA\t| Pneu #1\t| Pneu #2\t| Pneu #3\t| Pneu #4");
   }
 
+  /**
+   * Esse metodo e utilizado para imprimir todos os IDs registrados.
+   * 
+   * @return void
+   */
   public void printIds() {
     System.out.print("Lista de todos os IDs registrados: \n[");
-    for(int i=0; i<vehicles.size(); i++) {
+    for (int i = 0; i < vehicles.size(); i++) {
       System.out.print(" #" + vehicles.get(i).getId());
     }
     System.out.println(" ]");
   }
 
+  /**
+   * Esse metodo e utilizado para verificar se um ID ja esta registrado.
+   * 
+   * @param int - ID a ser verificado.
+   * 
+   * @return boolean
+   */
   public boolean validateId(int id) {
     return getVehicleById(id) != null;
   }
 
+  /**
+   * Esse metodo e utilizado para esvaziar um pneu.
+   * 
+   * @param int - ID do veiculo.
+   * @param int - ID do pneu.
+   * 
+   * @return void
+   * 
+   * @see Vehicle
+   * @see Tire
+   */
   public void emptyTire(int vehicleId, int tireId) {
     Vehicle vehicle = this.getVehicleById(vehicleId);
 
@@ -112,6 +217,16 @@ public class Simulator {
     vehicle.emptyTire(tireId);
   }
 
+  /**
+   * Esse metodo e utilizado para esvaziar todos os pneus de um veiculo.
+   * 
+   * @param int - ID do veiculo.
+   * 
+   * @return void
+   * 
+   * @see Vehicle
+   * @see Tire
+   */
   public void emptyAllTires(int vehicleId) {
     Vehicle vehicle = this.getVehicleById(vehicleId);
 
@@ -122,6 +237,17 @@ public class Simulator {
     vehicle.emptyAllTires();
   }
 
+  /**
+   * Esse metodo e utilizado para calibrar um pneu.
+   * 
+   * @param int - ID do veiculo.
+   * @param int - ID do pneu.
+   * 
+   * @return void
+   * 
+   * @see Vehicle
+   * @see Tire
+   */
   public void calibrateTire(int vehicleId, int tireId) {
     Vehicle vehicle = this.getVehicleById(vehicleId);
 
@@ -132,6 +258,16 @@ public class Simulator {
     vehicle.calibrateTire(tireId);
   }
 
+  /**
+   * Esse metodo e utilizado para calibrar todos os pneus de um veiculo.
+   * 
+   * @param int - ID do veiculo.
+   * 
+   * @return void
+   * 
+   * @see Vehicle
+   * @see Tire
+   */
   public void calibrateAllTires(int vehicleId) {
     Vehicle vehicle = this.getVehicleById(vehicleId);
 
@@ -142,12 +278,22 @@ public class Simulator {
     vehicle.calibrateAllTires();
   }
 
+  /**
+   * Esse metodo e utilizado para desenhar a pista.
+   * 
+   * @return void
+   */
   public void drawTrack() {
     for (Vehicle vehicle : this.vehicles) {
       vehicle.draw();
     }
   }
 
+  /**
+   * Esse metodo e utilizado para salvar os dados em um arquivo.
+   * 
+   * @return void
+   */
   public void saveData() {
     File file = new File("vehicles.dat");
 
@@ -165,14 +311,18 @@ public class Simulator {
     }
   }
 
-  @SuppressWarnings("unchecked")
+  /**
+   * Esse metodo e utilizado para carregar os dados de um arquivo.
+   * 
+   * @return void
+   */
   public void loadData() {
     File file = new File("vehicles.dat");
-    
+
     try {
       FileInputStream fileInputStream = new FileInputStream(file);
       ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
-      
+
       this.vehicles = (ArrayList<Vehicle>) objectInputStream.readObject();
 
       objectInputStream.close();
@@ -182,10 +332,26 @@ public class Simulator {
     }
   }
 
+  /**
+   * Esse metodo e utilizado para obter a lista de veiculos.
+   * 
+   * @return ArrayList<Vehicle> - Lista de veiculos.
+   * 
+   * @see Vehicle
+   */
   public ArrayList<Vehicle> getVehicles() {
     return this.vehicles;
   }
 
+  /**
+   * Esse metodo e utilizado para obter um veiculo pelo seu ID.
+   * 
+   * @param int - ID do veiculo.
+   * 
+   * @return Vehicle
+   * 
+   * @see Vehicle
+   */
   private Vehicle getVehicleById(int id) {
     for (Vehicle vehicle : this.vehicles) {
       if (vehicle.getId() == id) {
